eb7a5cc9e3e7e5f22fddbc5691a5db5aea7082d2
Rachel Lima <rachel.l@samsung.com>
2020-06-01 21:10:42 -0400

    Add init project
    Signed-off-by: Rachel Lima <rachel.l@samsung.com>

---
+++ Source/scapegame/scapegame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "scapegame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, scapegame, "scapegame" );
---
+++ Source/scapegame/scapegameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "scapegameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCAPEGAME_API AscapegameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
